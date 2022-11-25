3a5645c0718dafccf39d02fd454c30b031f49849
LaupWing <loc_strict@hotmail.com>
2022-11-25 16:00:40 +0100

    import all the stuf

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -2,10 +2,24 @@ import createSchema from "part:@sanity/base/schema-creator"
 
 import schemaTypes from "all:part:@sanity/base/schema-type"
 import testimonails from "./testimonails"
+import abouts from "./abouts"
+import brands from "./brands"
+import contact from "./contact"
+import experiences from "./experiences"
+import skills from "./skills"
+import workExperience from "./workExperience"
+import works from "./works"
 
 export default createSchema({
    name: "default",
    types: schemaTypes.concat([
-      testimonails
+      testimonails,
+      abouts,
+      brands,
+      contact,
+      experiences,
+      skills,
+      workExperience,
+      works,
    ]),
 })
