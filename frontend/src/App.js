d1585130e447bc5a28cedd3843eaeb04dee9349f
LaupWing <loc_strict@hotmail.com>
2022-11-26 11:57:08 +0100

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from "react"
+import { Navbar } from "./components"
+
+import { About, Footer, Header, Skills, Testimonials, Works } from "./container"
 
 const App = () => {
-   return <div>App</div>
+   return (
+      <div className="app">
+         <Navbar/>
+         <Header/>
+         <About/>
+         <Works/>
+         <Skills/>
+         <Testimonials/>
+         <Footer/>
+      </div>
+   )
 }
 
 export default App
