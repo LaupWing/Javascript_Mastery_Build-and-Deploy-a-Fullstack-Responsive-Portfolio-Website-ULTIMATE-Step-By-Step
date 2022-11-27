22718d9735d39870f1a241d2232f3fe1ca9d5934
LaupWing <loc_strict@hotmail.com>
2022-11-27 06:56:11 +0100

    update

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,9 +1,60 @@
 import React from "react"
+import { motion } from "framer-motion"
 
 import "./About.scss"
 
+const abouts = [
+   {
+      title: "Web Development",
+      description: "I am a good web developer",
+      imgUrl: "",
+   },
+   {
+      title: "Web Design",
+      description: "I am a good web developer",
+      imgUrl: "",
+   },
+   {
+      title: "UI/UX",
+      description: "I am a good web developer",
+      imgUrl: "",
+   },
+]
+
 const About = () => {
-   return <div>About</div>
+   return (
+      <>
+         <h2 className="head-text">
+            I know that
+            <span>Good Design</span>
+            <br />
+            means
+            <span>Good Business</span>
+         </h2>
+         <div className="app__profiles">
+            {abouts.map((about, index) => (
+               <motion.div
+                  whileInView={{ opacity: 1 }}
+                  whileHover={{ scale: 1.1 }}
+                  transition={{
+                     duration: 0.5,
+                     type: "tween",
+                  }}
+                  key={about.title}
+                  className="app__profile-item"
+               >
+                  <img src={about.imgUrl} alt={about.title} />
+                  <h2 className="bold-text" style={{ margintop: 20 }}>
+                     {about.title}
+                  </h2>
+                  <p className="p-text" style={{ margintop: 10 }}>
+                     {about.description}
+                  </p>
+               </motion.div>
+            ))}
+         </div>
+      </>
+   )
 }
 
 export default About
