c9dace155b7ec665ca3b60c0cd9ff976bddea921
LaupWing <loc_strict@hotmail.com>
2022-11-28 12:27:33 +0100

    udpate

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -4,10 +4,44 @@ import ReactTooltip from "react-tooltip"
 
 import { AppWrap, MotionWrap } from "../../wrapper"
 import { urlFor, client } from "../../client"
-import "./Skills.scss"  
+import "./Skills.scss"
 
 const Skills = () => {
-   return <div>Skills</div>
+   const [experiences, setExperiences] = useState([])
+   const [skills, setSkills] = useState([])
+
+   useEffect(()=>{
+      const query = "*[_type == 'experiences']"
+      const skillsQuery = "*[_type == 'skills']"
+
+      client.fetch(query)
+         .then(data=>{
+            setExperiences(data)
+         })
+      client.fetch(skillsQuery)
+         .then(data=>{
+            setSkills(data)
+         })
+   },[])
+
+   return (
+      <>
+         <h2 className="head-text">Skills & Experiences</h2>
+         <div className="app__skills-container">
+            <motion.div className="app__skills-list">
+               {skills.map(skill=>(
+
+               )}
+               <motion.div
+                  whileInView={{ opacity: [0, 1] }}
+                  transition={{ duration: 0.5 }}
+                  className="app__skills-item app__flex"
+                  key={skill.name}
+               ></motion.div>
+            </motion.div>
+         </div>
+      </>
+   )
 }
 
 export default Skills
